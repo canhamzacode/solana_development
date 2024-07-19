5ebcbcb83c1742a2591614cbd876e8fae448d136
canhamzacode <abdulmuizzayo6@gmail.com>
2024-07-19 22:02:16 +0200

    sending of sol imlemented sucessfully note the receivers address must also come from sol not faucet

--- 03_send_transactions.ts
+++ 03_send_transactions.ts
@@ -3,27 +3,24 @@ dotenv.config()
 import { clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
 import { getKeypairFromEnvironment } from '@solana-developers/helpers';
 
+
+
 // get the secret key from the environment simulating a more dynamic option
-
-const keypair = process.env.SECRET_KEY || null
-
-
-if (!keypair){
-    console.log("Pls provide a key pair");
-    process.exit(1);
-}
 
 // connect to cluster i.e devnet, testnet or mainnet
 const connection = new Connection(clusterApiUrl("devnet"));
 const sendersAddress = getKeypairFromEnvironment("SECRET_KEY");
-const receiverAddress = new PublicKey("0xDC9Fe849b822eE1763e4aC4a725f994A4365b179")
+const receiverAddress = new PublicKey("23wRQUTNurqBjnRYpwiSGKaUUAZTzi5so5mmhTgaJbWf")
+const publicKey = sendersAddress.publicKey;
 
 const amount = 0.3 * LAMPORTS_PER_SOL
+
+console.log(sendersAddress)
 
 const transaction = new Transaction()
 
 const transferInstruction = SystemProgram.transfer({
-    fromPubkey: sendersAddress.publicKey,
+    fromPubkey: publicKey,
     toPubkey: receiverAddress,
     lamports: amount,
 })
